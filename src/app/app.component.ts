01cc1c62d97295744e7ffc496a3f52a873caaf84
academy_learner <academy_learner@academy-learners-MacBook-Air.local>
2021-12-14 09:19:19 +0200

    project initialization

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//g
+export class AppComponent {
+  title = 'Angular Practice';
+}
